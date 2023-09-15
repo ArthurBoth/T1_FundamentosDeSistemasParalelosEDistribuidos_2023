771222fb707d579bd8ad0915948d874697126402
Arthur Both <arthur.both001@edu.pucrs.br>
2023-09-15 12:46:01 -0300

    Resolvidos os exercícios da pasta 4-NaoDeterminismo

--- 4-NaoDeterminismo/Ex1-tickBoom.go
+++ 4-NaoDeterminismo/Ex1-tickBoom.go
@@ -1,17 +1,17 @@
-// Disciplina de Modelos de Computacao Concorrente
-// Escola Politecnica - PUCRS
-// Prof.  Fernando Dotti
-// programa da internet
+/* Disciplina de Modelos de Computacao Concorrente
+   Escola Politecnica - PUCRS
+   Prof.  Fernando Dotti
+   programa da internet
 
-// a biblioteca time de go oferece a possibilidade de criar
-// canais que mandam sinais temporizados conforme parametro passado na criacao.
-//
-// Atenção: veja o comando nao deterministico (select case) reagindo a diversos canais
-//
-// Exercício:
-// 		crie outros canais para eventos temporizados e declare reacoes aos
-// 		mesmos junto ao mesmo select
-
+   a biblioteca time de go oferece a possibilidade de criar
+   canais que mandam sinais temporizados conforme parametro passado na criacao.
+  
+   Atenção: veja o comando nao deterministico (select case) reagindo a diversos canais
+  
+   Exercício:
+   		crie outros canais para eventos temporizados e declare reacoes aos
+   		mesmos junto ao mesmo select
+*/
 package main
 
 import (
@@ -21,11 +21,14 @@ import (
 
 func main() {
 	tick := time.Tick(100 * time.Millisecond)
+	tack := time.Tick(100 * time.Millisecond)
 	boom := time.After(500 * time.Millisecond)
 	for {
 		select {
 		case <-tick:
 			fmt.Println("tick.")
+		case <-tack:
+			fmt.Println("tack.")
 		case <-boom:
 			fmt.Println("BOOM!")
 			return
