09d2ef2f299db6cfb4a986d36c7516a82556b2a1
Arthur Both <arthur.both001@edu.pucrs.br>
2023-09-17 19:14:41 -0300

    Resolvidos os Exercícios da Pasta 10-Servidores

--- 10-Servidores/Ex2-Servidores2PERGUNTA.go
+++ 10-Servidores/Ex2-Servidores2PERGUNTA.go
@@ -1,19 +1,24 @@
-// por Fernando Dotti - fldotti.github.io - PUCRS - Escola Politécnica
-// servidor com criacao dinamica de thread de servico
-// Problema:
-//   considere um servidor que recebe pedidos por um canal (representando uma conexao)
-//   ao receber o pedido, sabe-se através de qual canal (conexao) responder ao cliente.
-//   Abaixo uma solucao sequencial para o servidor.
-// Exercicio
-//   deseja-se tratar os clientes concorrentemente, e nao sequencialmente.
-//   como ficaria a solucao ?
-// Veja abaixo a resposta ...
-//   quantos clientes podem estar sendo tratados concorrentemente ?
-//
-// Exercicio:
-//   agora suponha que o seu servidor pode estar tratando no maximo 10 clientes concorrentemente.
-//   como voce faria ?
-//
+/* por Arthur Antunes de Souza Both e Gabriel Moraes Ferreira
+
+   Problema:
+     considere um servidor que recebe pedidos por um canal (representando uma conexao)
+     ao receber o pedido, sabe-se através de qual canal (conexao) responder ao cliente.
+     Abaixo uma solucao sequencial para o servidor.
+   Exercicio
+     deseja-se tratar os clientes concorrentemente, e nao sequencialmente.
+     como ficaria a solucao ?
+   Veja abaixo a resposta ...
+     quantos clientes podem estar sendo tratados concorrentemente ?
+  
+   Exercicio:
+     agora suponha que o seu servidor pode estar tratando no maximo 10 clientes concorrentemente.
+     como voce faria ?
+	 	R: Limitamos a quantidade de requisições de cada cliente pode fazer (antes eram infinitas).
+		   Modificamos a função cliente para que ela receba mais um parâmetro e avise quando terminar.
+		   Forçamos o servidor a esperar um cliente terminar quando há mais de 10 clientes ativos.
+		   O resto do nosso código está separado do código original. No Main, está antes de um comentário
+		   e o restante está depois do Main.
+*/
 
 package main
 
@@ -25,7 +30,9 @@ import (
 const (
 	NCL  = 100
 	Pool = 10
+	ClientRequisitonLimit = 10
 )
+
 
 type Request struct {
 	v      int
@@ -34,15 +41,16 @@ type Request struct {
 
 // ------------------------------------
 // cliente
-func cliente(i int, req chan Request) {
+func cliente(i int, req chan Request, done chan struct{}) {
 	var v, r int
 	my_ch := make(chan int)
-	for {
+	for i := 0; i < ClientRequisitonLimit; i++ {
 		v = rand.Intn(1000)
 		req <- Request{v, my_ch}
 		r = <-my_ch
 		fmt.Println("cli: ", i, " req: ", v, "  resp:", r)
 	}
+	done <- struct{}{} // libera um slot no pool
 }
 
 // ------------------------------------
@@ -66,12 +74,42 @@ func servidorConc(in chan Request) {
 
 // ------------------------------------
 // main
+
+
 func main() {
+	fim := make(chan struct{})
+	treating := make(chan struct{}, Pool)
+	done := make(chan struct{}, Pool)
+	go applyServerLimit(treating, done, fim) // Ativa o limitador de threads
+	<- done // espera o limitador de threads estar pronto
+
+	// ------------------------------------
+
 	fmt.Println("------ Servidores - criacao dinamica -------")
-	serv_chan := make(chan Request) // CANAL POR ONDE SERVIDOR RECEBE PEDIDOS
-	go servidorConc(serv_chan)      // LANÇA PROCESSO SERVIDOR
-	for i := 0; i < NCL; i++ {      // LANÇA DIVERSOS CLIENTES
-		go cliente(i, serv_chan)
+	serv_chan := make(chan Request)
+	go servidorConc(serv_chan)
+	for i := 0; i < NCL; i++ {
+		<- treating							// Espera um slot no pool
+		go cliente(i, serv_chan, done)
 	}
-	<-make(chan int)
+	fim <- struct{}{}
+}
+//---------------------------------------------------------------------------------------------
+func applyServerLimit(treating chan struct{}, done chan struct{}, fim chan struct{}){
+	for i := 0; i < Pool; i++ {
+		treating <- struct{}{}
+	}
+	done <- struct{}{} // avisa o main que o limitador de threads está pronto
+	go limitPool(treating, done, fim)
+}
+func limitPool(treating chan struct{}, done chan struct{}, fim chan struct{}){
+	for i := 0; i < NCL;i++{
+		<- done				   // Espera uma requisição terminar
+		treating <- struct{}{} // libera um slot no pool
+	}
+	end(fim)
+}
+
+func end(fim chan struct{}){ // implementado para evitar uma finalização de programa através deadlock
+	<- fim
 }
--- 10-Servidores/ServidorHTTP/server.go
+++ 10-Servidores/ServidorHTTP/server.go
@@ -1,18 +1,18 @@
-// Disciplina de Modelos de Computacao Concorrente
-// Escola Politecnica - PUCRS
-// Prof.  Fernando Dotti
+/* Disciplina de Modelos de Computacao Concorrente
+   Escola Politecnica - PUCRS
+   Prof.  Fernando Dotti
 
-// rode o programa abaixo
-// em um browser na sua máquina entre:
-//     http://127.0.0.1:8080    ou
-//     http://127.0.0.1:8080/help   ou
-//     http://127.0.0.1:8080/f1
-//
-// as funcoes helloWorld, help e f1 abaixo serão respectivamente
-// ativadas no servidor, gerando a resposta enviada ao browser
-// tente entender o modelo subjascente de troca de mensagens e
-// tratamento de conexoes (canais)
-
+   rode o programa abaixo
+   em um browser na sua máquina entre:
+	   http://127.0.0.1:8080    ou
+	   http://127.0.0.1:8080/help   ou
+       http://127.0.0.1:8080/f1
+  
+   as funcoes helloWorld, help e f1 abaixo serão respectivamente
+   ativadas no servidor, gerando a resposta enviada ao browser
+   tente entender o modelo subjascente de troca de mensagens e
+   tratamento de conexoes (canais)
+*/
 package main
 
 import (
